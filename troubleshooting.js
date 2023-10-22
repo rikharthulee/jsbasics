47a9e0fdcd3db9e468c3da3ce982c8868224a90c
rikharhthulee <rikharhthulee@gmail.com>
2023-10-22 01:07:49 +0100

    updated comments

--- troubleshooting.js
+++ troubleshooting.js
@@ -2,16 +2,18 @@
  * ===== Troubleshooting =====
  * The function below should log the number 2, however it does not, 
  * see if you can fix it!
- * Be sure to fix it in the spirit of the code, do not hard code the result.
+ * 
+ * The original issue was that a and b were set as string values so the result was oringinally "ab".  I have taken the problem from replit and recreated it here in this JS file.
+ * 
  */
 
-function troubleshooting(result) {
+function troubleshooting(answer) {  
 	const a = 1
 	const b = 1
 
 
 
-	return a + b
+	return a + b // return command sends the answer between the brackets above.  It doesn't matter that I have called it "answer" it works even if it is blank 
 }
 
-console.log(troubleshooting())
+console.log(troubleshooting())  // this is saying print out the returned value from the troubleshooting funtion, in this case the answer of 2
