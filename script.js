c0eebf95d4448df7888edd735010abc67f5680ba
rikharhthulee <rikharhthulee@gmail.com>
2023-10-22 01:01:23 +0100

    recreated exercisies from fundementals odin

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ console.log("Hello World") // Output message to console
 console.log(1+1) // Basic addition 
 
 console.log((4 + 6 + 9) / 77) // equation
+
+let a = 10 // declare a variable and assign the value of 10
+console.log(a) // print the value of variable a
+
+console.log(9 * a) // 9 x variable a = 90
+
+let b = 7 * a
+
+console.log(b) // b = 7 * a (10) = 70 - note that this would come back as undefined if let a wasn't declared above
---
+++ troubleshooting.js
@@ -0,0 +1,17 @@
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting(result) {
+	const a = 1
+	const b = 1
+
+
+
+	return a + b
+}
+
+console.log(troubleshooting())
